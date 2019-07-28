225181ee63743495fcac20c4f2a054ae3e9a83bc
myname21-meet <myname21@meet.mit.edu>
2019-07-28 09:14:29 -0400

    oh oh

--- part2.py
+++ part2.py
@@ -1,7 +1,26 @@
 # Part 2 of the Python Review lab.
+def prime(x):
+	for i in range(2,x):
+		if (x % i) == 0:
+			print(x,"is not a prime number")
+			break
+
+		else:
+			print(x,"is a prime number")
+			break
+
+
 
 def encode(x, y):
-	pass
+	prime(x)
+	if 1 < y < 250 and 500 < x < 1000 :
+
+		return(x*y)
+	else:
+		print ('Invalid input: Outside range.')
+
+	
+
 
 def decode(coded_message):
 	pass
